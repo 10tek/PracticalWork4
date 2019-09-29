8be98ef4f6009a327a909503473f2d0e2ce1f298
10tekinvo@gmail.com <10tekinvo@gmail.com>
2019-09-29 18:55:59 +0600

    Cоздал класс Money с cash и с различными методами

---
+++ PW4/Money.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PW4
+{
+    public class Money
+    {
+        private string _denomination;
+        private int _cash;
+
+        public int Cash
+        {
+            get
+            {
+                return _cash;
+            }
+        }
+
+        public Money() { }
+
+        public Money(string denomination, int cash)
+        {
+            _denomination = denomination;
+            _cash = cash;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Номинал: {_denomination}; Кол-во денег: {Cash}");
+        }
+
+        public bool IsEnough(int productCost)
+        {
+            if (Cash >= productCost)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public int HowMuch(int productCost)
+        {
+            return Cash / productCost;
+        }
+    }
+}
